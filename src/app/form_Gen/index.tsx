9bbd29262447b8049dfd3eb3497fbcfdcec854ce
AnukulPr1me <anukul_chandra@outlook.com>
2024-05-06 17:33:19 +0530

    implemented Open AI api

--- src/app/form_Gen/index.tsx
+++ src/app/form_Gen/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, {useState}from "react";
+import React, { useState, useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
@@ -9,35 +9,67 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-  DialogFooter
+  DialogFooter,
 } from "@/components/ui/dialog";
 import { Textarea } from "@/components/ui/textarea";
-type Props={}
+import { generateForm } from "@/actions/generateForm";
+import { useFormState, useFormStatus } from "react-dom";
+type Props = {};
+
+const initialState: {
+  message: string;
+  data?: any;
+} = {
+  message: "",
+};
+export function SubmitButton() {
+  const { pending } = useFormStatus();
+  return (
+    <Button type="submit" disabled={pending}>
+      {pending ? "Generating..." : "Generate"}
+    </Button>
+  );
+}
 
 const FormGenerator = (props: Props) => {
-    const [open, setOpen] = useState(false);
-    const onFormCreate = () => {
-        setOpen(true);
+  const [state, formAction] = useFormState(generateForm, initialState);
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    if (state.message === "success") {
+      setOpen(false);
     }
+    console.log(state.data);
+  }),
+    [state.message];
+  const onFormCreate = () => {
+    setOpen(true);
+  };
   return (
     <Dialog open={open} onOpenChange={setOpen}>
-        <Button onClick={onFormCreate}>Create Form</Button>
+      <Button onClick={onFormCreate}>Create Form</Button>
       <DialogContent className="sm:max-w-[425px]">
         <DialogHeader>
           <DialogTitle>Create new Form</DialogTitle>
         </DialogHeader>
-        <form>
-            <div className="grip gap-4 py-4">
-                <Textarea id="description" name="description" required placeholder="Share what your form is about who is it for
-                and what information it has and what do you want to collect with it"></Textarea>
-            </div>
+        <form action={formAction}>
+          <div className="grip gap-4 py-4">
+            <Textarea
+              id="description"
+              name="description"
+              required
+              placeholder="Share what your form is about who is it for
+                and what information it has and what do you want to collect with it"
+            ></Textarea>
+          </div>
+          <DialogFooter>
+            <SubmitButton />
+            <Button variant="link">Create Manully</Button>
+          </DialogFooter>
         </form>
-        <DialogFooter>
-            <Button variant='link'>Create Manully</Button>
-        </DialogFooter>
       </DialogContent>
     </Dialog>
   );
-}
+};
 
 export default FormGenerator;
